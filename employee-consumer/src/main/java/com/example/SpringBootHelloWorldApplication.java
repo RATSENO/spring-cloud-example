6b807f634e60de8d57176d97b8373dbf1c58dbc8
RATSENO <onestar1014@gmail.com>
2020-01-23 11:13:00 +0900

    robbin을 이용한 로드 밸런싱

--- employee-consumer/src/main/java/com/example/SpringBootHelloWorldApplication.java
+++ employee-consumer/src/main/java/com/example/SpringBootHelloWorldApplication.java
@@ -13,17 +13,23 @@ import java.io.IOException;
 public class SpringBootHelloWorldApplication {
 
     public static void main(String[] args) throws RestClientException, IOException {
+
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+        /*
+        서버 스타스 시 자동으로 호출하는 부분
         ApplicationContext ctx = SpringApplication.run(
                 SpringBootHelloWorldApplication.class, args);
 
         ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
         System.out.println(consumerControllerClient);
         consumerControllerClient.getEmployee();
+        */
     }
 
-    @Bean
+    /*@Bean
     public  ConsumerControllerClient  consumerControllerClient()
     {
         return  new ConsumerControllerClient();
     }
+    */
 }
--- employee-consumer/src/main/java/com/example/controller/ConsumerControllerClient.java
+++ employee-consumer/src/main/java/com/example/controller/ConsumerControllerClient.java
@@ -6,26 +6,36 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-@Controller
+//@Controller
+@RestController
 public class ConsumerControllerClient {
 
     @Autowired
-    private DiscoveryClient discoveryClient;
+    //private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
 
+    @GetMapping("/eureka/client")
     public void getEmployee() throws RestClientException, IOException {
-
+        /*
         List<ServiceInstance> instances=discoveryClient.getInstances("employee-producer");
         ServiceInstance serviceInstance=instances.get(0);
-
         String baseUrl=serviceInstance.getUri().toString();
+        */
+        ServiceInstance serviceInstance = loadBalancerClient.choose("employee-producer");
+        System.out.println("#####service-instance-uri:"+serviceInstance.getUri());
+        String baseUrl = serviceInstance.getUri().toString();
 
         baseUrl=baseUrl+"/employee";
 
